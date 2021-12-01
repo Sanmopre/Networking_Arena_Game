328b072a279efd28bc358d98def107b2322b2a53
Maffat <ramidavidR2@gmail.com>
2021-12-01 20:01:12 +0100

    Tested and fixed reliability and added Package Loss and Jitter Simulation
    -works on the test environment
    -pendant to be implemented in the client and server code

---
+++ Networking_Arena_Game/Assets/Scirpts/Network/Test.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net;
+using System.Text;
+
+public class Test : MonoBehaviour
+{
+    readonly EndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6969);
+    readonly EndPoint nullAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6970);
+
+    UDP socket = null;
+
+    public bool server = false;
+
+    string myName = "Server";
+    string message = "FromServer";
+
+    void Start()
+    {
+        socket = gameObject.GetComponent<UDP>();
+
+        if (!server)
+        {
+            myName = "Client";
+            message = "FromClient";
+            socket.SetRemoteAddress(serverAddress);
+        }
+        else
+            socket.SetRemoteAddress(nullAddress);
+    }
+    int messagesSent = 0;
+    public int messagesToSend = 25;
+    void Update()
+    {
+        while (socket.CanReceive())
+        {
+            string recv = socket.Receive(true);
+            if (recv == null)
+            {
+                Debug.Log(myName + " receive Error");
+                continue;
+            }
+            if (recv.Length == 0)
+                continue;
+        }
+        if (messagesSent < messagesToSend)
+        {
+            socket.Send(message);
+            ++messagesSent;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        socket.Close();
+    }
+
+    IEnumerator SendDelay()
+    {
+        yield return new WaitForSeconds(1.0f);
+        socket.Send(message);
+    }
+}
--- Networking_Arena_Game/Assets/Scirpts/Network/UDP.cs
+++ Networking_Arena_Game/Assets/Scirpts/Network/UDP.cs
@@ -11,17 +11,20 @@ using UnityEngine;
 
 public class UDP : MonoBehaviour
 {
+    public int port = 0;
+
     [HideInInspector]
     static public readonly int MAX_BUFFER = 1300;
     [HideInInspector]
     static public readonly byte[] DISCONNECT = new byte[1];
     [HideInInspector]
-    static public readonly float MSG_WAIT_TIME = 2.0f;
+    static public readonly float MSG_WAIT_TIME = 3.0f;
 
     Socket thisSocket = null;
     EndPoint remoteAddress = null;
     int messageID = 0;
 
+    // --- Message ---
     struct Message
     {
         public Message(int id, byte[] data)
@@ -31,58 +34,50 @@ public class UDP : MonoBehaviour
             start = Time.realtimeSinceStartup;
         }
 
-        public bool IsTimedOut()
-        {
-            if (Time.realtimeSinceStartup > start + MSG_WAIT_TIME)
-                return true;
-            return false;
-        }
-
-        public void RestartTimer()
-        {
-            start = Time.realtimeSinceStartup;
-        }
-
         readonly int id;
+
         public int GetID() { return id; }
         public byte[] data;
-        float start;
+        public float start;
     }
-    List<Message> notAcknoledged = new List<Message>();
-    int allReceivedLast = -1;
-    List<Message> alreadyReceived = new List<Message>();
 
-    public void Start()
+    bool MessageIsTimedOut(Ref<Message> message)
+    {
+        if (Time.realtimeSinceStartup > message.value.start + MSG_WAIT_TIME)
+            return true;
+        return false;
+    }
+
+    void MessageRestartTimer(Ref<Message> message)
+    {
+        message.value.start = Time.realtimeSinceStartup;
+    }
+    // --- !Message ---
+    List<Ref<Message>> notAcknoledged = new List<Ref<Message>>();
+
+    int lastID = -1;
+    List<int> receivedIDs = new List<int>();
+
+    public void Awake()
     {
         thisSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         thisSocket.Blocking = false;
-        IPEndPoint thisAddress = new IPEndPoint(IPAddress.Any, 0);
+        IPEndPoint thisAddress = new IPEndPoint(IPAddress.Any, port);
 
         thisSocket.Bind(thisAddress);
 
-        // ---
-
-        alreadyReceived.Add(new Message(-2, null));
+        // --- Shady stuffy ---
+        thread = new Thread(SendMessages);
+        thread.Start();
     }
 
     public void Update()
     {
         for (int i = 0; i < notAcknoledged.Count; ++i)
-            if (notAcknoledged[i].IsTimedOut())
+            if (MessageIsTimedOut(notAcknoledged[i]))
             {
                 Resend(notAcknoledged[i]);
-                notAcknoledged.RemoveAt(i);
-                --i;
-                continue;
-            }
-
-        for (int i = 1; i < alreadyReceived.Count; ++i)
-            if (alreadyReceived[i].IsTimedOut())
-            {
-                Resend(alreadyReceived[i]);
-                alreadyReceived.RemoveAt(i);
-                --i;
-                continue;
+                MessageRestartTimer(notAcknoledged[i]);
             }
     }
 
@@ -91,7 +86,7 @@ public class UDP : MonoBehaviour
         remoteAddress = toAddress;
     }
 
-    public byte[] Receive(bool setRemote = false)
+    public string Receive(bool setRemote = false)
     {
         byte[] inputPacket = new byte[MAX_BUFFER];
         EndPoint from = new IPEndPoint(IPAddress.None, 0);
@@ -113,7 +108,7 @@ public class UDP : MonoBehaviour
             else
             {
                 ReportError("Receive Error: Received a message from an unknown address");
-                return new byte[0];
+                return "";
             }
 
         if (bytesRecv == 1)
@@ -123,68 +118,76 @@ public class UDP : MonoBehaviour
         }
 
         int idSize = sizeof(int);
-        int recvID = BitConverter.ToInt32(inputPacket, idSize);
+        int recvID = BitConverter.ToInt32(inputPacket, 0);
 
         if (bytesRecv != idSize) // check if it is an acknoledgement package
         {
-            if (recvID <= allReceivedLast)
-                return new byte[0];
-            for (int i = 1; i < alreadyReceived.Count; ++i)
+            Debug.Log(name + " Received Message -> " + recvID);
+            if (recvID <= lastID)
             {
-                if (recvID == alreadyReceived[i].GetID())
-                {
-                    alreadyReceived[i].RestartTimer();
-                    return new byte[0];
-                }
+                SendAcknowledgement(recvID);
+                return "";
             }
-
-            if (alreadyReceived.Count > 1)
-            {
-                for (int i = 1; i < alreadyReceived.Count; ++i)
+            for (int i = 0; i < receivedIDs.Count; ++i)
+                if (recvID == receivedIDs[i])
                 {
-                    if (alreadyReceived[i].GetID() > recvID)
+                    SendAcknowledgement(recvID);
+                    return "";
+                }
+
+            if (receivedIDs.Count > 0)
+            {
+                for (int i = 0; i < receivedIDs.Count; ++i)
+                {
+                    if (receivedIDs[i] > recvID)
                     {
-                        alreadyReceived.Insert(i - 1, new Message(recvID, null));
+                        receivedIDs.Insert(i, recvID);
+                        break;
+                    }
+                    if (i == receivedIDs.Count - 1)
+                    {
+                        receivedIDs.Add(recvID);
                         break;
                     }
                 }
             }
             else
-                alreadyReceived.Add(new Message(recvID, null));
+                receivedIDs.Add(recvID);
 
-            int nextID = recvID;
-            while (nextID - 1 == allReceivedLast)
+            int nextID = receivedIDs[0];
+            while (nextID - 1 == lastID)
             {
-                allReceivedLast = nextID;
+                lastID = nextID;
 
-                alreadyReceived.RemoveAt(1);
-                if (alreadyReceived.Count == 1)
+                receivedIDs.RemoveAt(0);
+                if (receivedIDs.Count == 0)
                     break;
 
-                nextID = alreadyReceived[1].GetID();
+                nextID = receivedIDs[0];
             }
 
-            Send(Encoding.UTF8.GetString(BitConverter.GetBytes(recvID)), true);
+            SendAcknowledgement(recvID);
         }
         else
         {
+            Debug.Log(name + " Acknowledged -> " + recvID);
             for (int i = 0; i < notAcknoledged.Count; ++i)
-                if (notAcknoledged[i].GetID() == recvID)
+                if (notAcknoledged[i].value.GetID() == recvID)
                 {
                     notAcknoledged.RemoveAt(i);
                     break;
                 }
-            return null;
+            return "";
         }
 
         byte[] toReceive = new byte[inputPacket.Length - idSize];
         for (int i = 0; i < toReceive.Length; ++i)
             toReceive[i] = inputPacket[i + idSize];
 
-        return toReceive;
+        return Encoding.UTF8.GetString(toReceive).TrimEnd('\0');
     }
 
-    public bool Send(string output, bool acknoledgePacket = false)
+    public bool Send(string output)
     {
         if (output.Length > MAX_BUFFER)
         {
@@ -205,22 +208,20 @@ public class UDP : MonoBehaviour
         }
         else
             outputPacket = DISCONNECT;
-        Message message = new Message(messageID, outputPacket);
 
-        byte[] toSend;
-        if (!acknoledgePacket)
-        {
-            byte[] header = BitConverter.GetBytes(message.GetID());
-            toSend = new byte[header.Length + outputPacket.Length];
-            header.CopyTo(toSend, 0);
-            outputPacket.CopyTo(toSend, header.Length);
-        }
-        else
-            toSend = outputPacket;
+        Ref<Message> message = new Ref<Message> { value = new Message(messageID, null) };
 
+        byte[] header = BitConverter.GetBytes(message.value.GetID());
+        byte[] toSend = new byte[header.Length + outputPacket.Length];
+        header.CopyTo(toSend, 0);
+        outputPacket.CopyTo(toSend, header.Length);
+
+        message.value.data = toSend;
+
+        Debug.Log(name + " Sent Message -> " + message.value.GetID());
         try
         {
-            thisSocket.SendTo(toSend, remoteAddress);
+            SendMessage(toSend, remoteAddress);
         }
         catch (SocketException error)
         {
@@ -228,40 +229,74 @@ public class UDP : MonoBehaviour
             return false;
         }
 
-        if (!acknoledgePacket)
-        {
-            notAcknoledged.Add(message);
-            messageID++;
-        }
+        notAcknoledged.Add(message);
+        messageID++;
+
         return true;
     }
 
-    bool Resend(Message outputMessage)
+    bool SendAcknowledgement(int id)
     {
         if (remoteAddress == null)
         {
-            ReportError("Networking Send Error: Remote Address is null");
+            ReportError("Networking Send Acknowledgement Error: Remote Address is null");
             return false;
         }
 
+        Debug.Log(name + " Sent Acknowledgement -> " + id);
         try
         {
-            thisSocket.SendTo(outputMessage.data, remoteAddress);
+            SendMessage(BitConverter.GetBytes(id), remoteAddress);
         }
         catch (SocketException error)
         {
-            ReportError("Networking Send Error: " + error.Message);
+            ReportError("Networking Send Acknowledgement Error: " + error.Message);
             return false;
         }
 
-        outputMessage.RestartTimer();
-        notAcknoledged.Add(outputMessage);
         return true;
+    }
+
+    bool Resend(Ref<Message> outputMessage)
+    {
+        if (remoteAddress == null)
+        {
+            ReportError("Networking Resend Error: Remote Address is null");
+            return false;
+        }
+
+        Debug.Log(name + " Resent -> " + outputMessage.value.GetID());
+        try
+        {
+            SendMessage(outputMessage.value.data, remoteAddress);
+        }
+        catch (SocketException error)
+        {
+            ReportError("Networking Resend Error: " + error.Message);
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool CanReceive()
+    {
+        if (thisSocket.Poll(0, SelectMode.SelectRead))
+            return true;
+        return false;
+    }
+
+    public bool CanSend()
+    {
+        if (thisSocket.Poll(0, SelectMode.SelectWrite))
+            return true;
+        return false;
     }
 
     public void Close()
     {
-        thisSocket.Close();
+        if (thisSocket != null)
+            thisSocket.Close();
     }
 
     void ReportError(string error)
@@ -271,6 +306,80 @@ public class UDP : MonoBehaviour
 
     private void OnDestroy()
     {
-        thisSocket.Close();
+        Close();
+
+        // --- Shady stuffy ---
+        thread.Abort();
+    }
+
+    // --- Shady stuffy ---
+
+    public bool jitter = true;
+    public bool packetLoss = true;
+    public int minJitt = 0;
+    public int maxJitt = 800;
+    public int lossThreshold = 90;
+
+    static readonly object myLock = new object();
+    Thread thread = null;
+
+    public struct Packet
+    {
+        public byte[] packet;
+        public DateTime time;
+        public UInt32 id;
+        public EndPoint ip;
+    }
+    public List<Packet> packetBuffer = new List<Packet>();
+
+    void SendMessage(byte[] text, EndPoint ip)
+    {
+        System.Random ran = new System.Random();
+        if (((ran.Next(0, 100) > lossThreshold) && packetLoss) || !packetLoss) // Don't schedule the message with certain probability
+        {
+            Packet pack = new Packet();
+            pack.packet = text;
+
+            if (jitter)
+                pack.time = DateTime.Now.AddMilliseconds(ran.Next(minJitt, maxJitt)); // delay the message sending according to parameters
+            else
+                pack.time = DateTime.Now;
+
+            pack.id = 0;
+            pack.ip = ip;
+            lock (myLock)
+                packetBuffer.Add(pack);
+        }
+        else
+            Debug.Log("Package Lost BOOHOO");
+    }
+
+    //Run this always in a separate Thread, to send the delayed messages
+    void SendMessages()
+    {
+        while (true)
+        {
+            DateTime date = DateTime.Now;
+            int i = 0;
+            if (packetBuffer.Count > 0)
+            {
+                List<Packet> auxBuffer;
+                lock (myLock)
+                    auxBuffer = new List<Packet>(packetBuffer);
+
+                foreach (var pack in auxBuffer)
+                {
+                    if (pack.time < date)
+                    {
+                        thisSocket.SendTo(pack.packet, pack.packet.Length, SocketFlags.None, pack.ip);
+                        lock (myLock)
+                            packetBuffer.RemoveAt(i);
+
+                        i--;
+                    }
+                    i++;
+                }
+            }
+        }
     }
 }
