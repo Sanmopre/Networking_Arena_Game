2e0e2b79ee470351bf4db6950c4a7cc87027c1ce
Aitorlb7 <45202115+Aitorlb7@users.noreply.github.com>
2021-12-18 11:22:12 +0100

    Enemy animations (Not tested yet)

--- Networking_Arena_Game/Assets/Scirpts/Gameplay/Enemy_Controller.cs
+++ Networking_Arena_Game/Assets/Scirpts/Gameplay/Enemy_Controller.cs
@@ -5,21 +5,21 @@ public class Enemy_Controller : MonoBehaviour
 {
     public string username = "null";
     int animationState = -1;
-    
-    
-    
+
+
+
     //TODO Aitor
     public void SetAnimationState(int state) { animationState = state; }
-
+    private Animator animator;
     // Use this for initialization
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        animator.SetInteger("State", animationState);
     }
 }
